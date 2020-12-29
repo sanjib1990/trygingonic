a449d0eb9186deb5d52e59e4f94e9ea2ce435aa4
sanjib1990 <devnath.sanjib@gmail.com>
2020-12-29 20:30:02 +0530

    Change

--- app/routes.go
+++ app/routes.go
@@ -37,4 +37,11 @@ func RegisterRoutes(engine *gin.Engine) {
             "results": []int{},
         }, 200, "Success")
     })
+
+    engine.POST("/api/suggestions/suggest", func(c *gin.Context) {
+        Logger.Get().Info("This is INFO")
+        response.Send(c, gin.H{
+            "results": []int{},
+        }, 200, "Success")
+    })
 }
